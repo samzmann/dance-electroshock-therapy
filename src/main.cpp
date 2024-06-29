f191b2299af2f1ef9485b7cafd862cf7b637d505
Samuel Mann <samuelcmann@gmail.com>
2024-06-29 13:25:36 +0200

    add BeatDetector

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
+#include "BeatDetector.h"
 
 const int myInput = AUDIO_INPUT_MIC;
 
@@ -24,14 +25,16 @@ const int myInput = AUDIO_INPUT_MIC;
 //
 AudioInputI2S audioInput; // audio shield: mic or line-in
 AudioSynthWaveformSine sinewave;
-AudioAnalyzeFFT1024 myFFT;
+AudioAnalyzeFFT256 myFFT;
+// AudioAnalyzeFFT1024 myFFT;
 AudioOutputI2S audioOutput; // audio shield: headphones & line-out
 
-// Connect either the live input or synthesized sine wave
+// Connect to the live input
 AudioConnection patchCord1(audioInput, 0, myFFT, 0);
-// AudioConnection patchCord1(sinewave, 0, myFFT, 0);
 
 AudioControlSGTL5000 audioShield;
+
+BeatDetector beatDetector(myFFT);
 
 void setup()
 {
@@ -43,40 +46,23 @@ void setup()
   audioShield.enable();
   audioShield.inputSelect(myInput);
   audioShield.volume(0.5);
+  audioShield.micGain(30);
 
   // Configure the window algorithm to use
   myFFT.windowFunction(AudioWindowHanning1024);
   // myFFT.windowFunction(NULL);
 
-  // Create a synthetic sine wave, for testing
-  // To use this, edit the connections above
-  sinewave.amplitude(0.8);
-  sinewave.frequency(1034.007);
+  // Initialize BeatDetector
+  beatDetector.enableSerialBeatDisplay = false;
 }
 
 void loop()
 {
-  float n;
-  int i;
 
-  if (myFFT.available())
+  beatDetector.BeatDetectorLoop();
+
+  if (beatDetector.lowBeat) // Beat has been detected
   {
-    // each time new FFT data is available
-    // print it all to the Arduino Serial Monitor
-    Serial.print("FFT: ");
-    for (i = 0; i < 40; i++)
-    {
-      n = myFFT.read(i);
-      if (n >= 0.01)
-      {
-        Serial.print(n);
-        Serial.print(" ");
-      }
-      else
-      {
-        Serial.print("  -  "); // don't print "0.00"
-      }
-    }
-    Serial.println();
+    Serial.println("Low frequency beat detected!");
   }
 }
